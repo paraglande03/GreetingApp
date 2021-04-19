689561692e2927150cc4039442b7ea4ab2b78119
paraglande03 <paraglande03@gmail.com>
2021-04-19 17:49:15 +0530

    Refactored: URL mapping for post method

--- src/main/java/com/parag/greeting/GreetingController.java
+++ src/main/java/com/parag/greeting/GreetingController.java
@@ -1,8 +1,6 @@
 package com.parag.greeting;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 @RestController
@@ -13,5 +11,9 @@ public class GreetingController {
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
         return new Greeting(counter.incrementAndGet(), String.format(template,name));
+    }
+    @PostMapping("/post")
+    public String grettingPost(@RequestBody Greeting greeting){
+        return greeting.getId()+ " "+ greeting.getContent()+" !!";
     }
 }
