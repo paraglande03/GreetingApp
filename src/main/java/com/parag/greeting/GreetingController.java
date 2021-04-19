3cbdaf6c572f31e0e9819f7906c5b45835e8ba0f
paraglande03 <paraglande03@gmail.com>
2021-04-19 18:33:01 +0530

    Refactored: Controller for HTTP methods

--- src/main/java/com/parag/greeting/GreetingController.java
+++ src/main/java/com/parag/greeting/GreetingController.java
@@ -16,6 +16,12 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter =new AtomicLong();
 
+
+    @RequestMapping(value = "/query", method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "fname") String fName, @RequestParam(value = "lName") String lName){
+            return "Hello" + fName + " "+ lName;
+    }
+
     @GetMapping(value = {"","/","/home"})
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
         return new Greeting(counter.incrementAndGet(), String.format(template,name));
